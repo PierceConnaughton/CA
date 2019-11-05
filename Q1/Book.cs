bbd2cbbcbb9eaf9e0baf9a8dfe42e63d40a0b0f5
Pierce Connaughton <S00185812@mail.itsligo.ie>
2019-11-05 16:11:53 +0000

    Add project files.

---
+++ Q1/Book.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    /*3.	Add an enum called Genre which has the following options – Fiction/Biography/Computing/Literature
+     */
+
+        //our enum genre 
+    enum Genre{
+        Fiction,
+        Biography,
+        Computing,
+        Lierature
+
+    }
+
+    /*4.	Add the following Properties to the Book class – Title[string], Author[string], Published[DateTime], Pages[int] and Genre[Genre].
+     */
+
+    class Book :IComparable
+    {
+        #region Properties
+        //our properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public int Pages { get; set; }
+        public Genre Genre { get; set; }
+        #endregion Properties
+
+        #region Constructors
+        //constructor with nothing in it if you havent any idea of what your gonna put in the object
+        public Book()
+        {
+
+        }
+
+        //constructor with only 2 properties as arguments if you only know the author and title
+        public Book(string title,string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        //constructor with all properties as arguments if you know all the info beforehand
+        public Book(string title,string author,DateTime published,int pages,Genre genre)
+        {
+
+            Title = title;
+            Author = author;
+            Published = published;
+            Pages = pages;
+            Genre = genre;
+        }
+        #endregion Constructors
+
+        #region Methods
+        /*6.	Override the ToString() method to display all properties. 
+         */
+
+        // to string method too write all information about the object
+        public override string ToString()
+        {
+            return string.Format("{0,-25}{1,-25}{2,-25}{3,-25}{4,-25}",  Title,  Author,  Published, Pages,  Genre);
+
+        }
+
+        //compares this object with the next object in the array
+        public int CompareTo(object obj)
+        {
+            Book That = (Book)obj;
+            //if the author in this object is same as the next one it will focus on the title to compare 
+            if (this.Author == That.Author)
+            {
+                return this.Title.CompareTo(That.Title);
+            }
+            //if authors are not the same it will comapre the authors
+            else
+            {
+                return this.Author.CompareTo(That.Author);
+            }
+            
+            
+                
+                
+        }
+        #endregion Methods
+    }
+}
---
+++ Q1/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Author Pierce Connaughton 
+ * 05/11/2019
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        /*1.	Create a new repo on Github and link it with your project in Visual Studio.  
+         * Make sure the repo is public so that I can view it.  
+         * If you are unsure how to do this just code the solution without linking to Github
+         * 
+         * 7.	Create 5 Book objects, you can use your own data or that in the screenshot.
+         * 8.	Add these Book objects to a List called ReadingList
+         * 9.	Write a method in Program.cs which displays the books in the reading list.
+         *
+         */
+        static void Main(string[] args)
+        {
+            //entering dates for each book
+            DateTime date1 = new DateTime(1895, 02, 12);
+            DateTime date2 = new DateTime(1945, 03, 11);
+            DateTime date3 = new DateTime(2011, 02, 23);
+            DateTime date4 = new DateTime(2001, 05, 29);
+            DateTime date5 = new DateTime(1495, 03, 31);
+
+            //entering in data for all the objects
+            Book book1 = new Book("IT", "Stephen King", date1, 200,Genre.Fiction );
+            Book book2 = new Book("Carrie", "Stephen King", date2, 150, Genre.Fiction);
+            Book book3 = new Book("Life Of Pi", "Jerry", date3, 232, Genre.Biography);
+            Book book4 = new Book("Coding", "Michael Richards", date4, 123, Genre.Computing);
+            Book book5 = new Book("Master Literature", "Arthur Phelp", date5, 89, Genre.Lierature);
+
+            //putting objects in a list
+            var ReadingList = new List<Book>();
+            ReadingList.Add(book1);
+            ReadingList.Add(book2);
+            ReadingList.Add(book3);
+            ReadingList.Add(book4);
+            ReadingList.Add(book5);
+
+            display(ReadingList);
+            Console.WriteLine();
+
+            //sorting list and displaying it in alphabetical order
+            ReadingList.Sort();
+            display(ReadingList);
+
+            Console.WriteLine();
+            Console.WriteLine("List with ages");
+            displayAge(ReadingList);
+
+        }
+        #region Methods
+        static void display(List<Book> bookList)
+        {
+            Console.WriteLine("{0,-25}{1,-25}{2,-25}{3,-25}{4,-25}","Title","Author","Published","Pages","Genre");
+            //for each book object in the list of books write out its properies
+            foreach (Book book in bookList)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+
+        static void displayAge(List<Book> bookList)
+        {
+
+            DateTime zeroTime = new DateTime(1, 1, 1);
+
+            Console.WriteLine("{0,-25}{1,-25}{2,-25}{3,-25}{4,-25}{5,-25}", "Title", "Author", "Published", "Pages", "Genre","Age");
+            foreach (Book book in bookList)
+            {
+                //getting the date now and the date of book publish and finding the time span of it and finding how many years the book is old from the date now
+                DateTime dateNow = DateTime.Now;
+                DateTime b = book.Published;
+                TimeSpan age = b - dateNow;
+                int ages = (zeroTime - age).Year - 1;
+
+                Console.WriteLine(book.ToString() + ages);
+            }
+        }
+        #endregion Methods
+    }
+}
